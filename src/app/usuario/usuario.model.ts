2895fb1bcdba46fba2bc7d78522d4823493099e3
yangoff <yangovip@gmail.com>
2017-08-11 18:14:21 -0300

    logica usuario

--- src/app/usuario/usuario.model.ts
+++ src/app/usuario/usuario.model.ts
@@ -1,11 +1,15 @@
-
 import { Oficina } from "../oficinas/oficinas.model";
+import { Mac } from "../mac/mac.model";
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 export class Usuario{
     private bluetoothMAC: string;
     private cpf: string;
     private nome: string;
     private oficinas: Oficina[];
-    
+    public cacheMAC: Mac[];
+    public saidaMAC: Mac[];
     constructor(){
 
     }
@@ -44,5 +48,26 @@ export class Usuario{
 	}
 //METHODS
 
+public marcaEntrada(obs:Observable<Mac[]>){
+    if(this.cacheMAC.length == 0){
+        obs.subscribe(macs=>{
+            macs.forEach((mac)=>{
+                this.cacheMAC.push(mac)
+            })
+            
+        })
+    }else{
+        obs.subscribe(macs=>{
+            macs.forEach((mac)=>{
+                if(!mac+"" in this.cacheMAC){
+                   this.saidaMAC.push(mac) 
+                }
+            })
+            
+        })
+    }
+
+
+}
 
 }
