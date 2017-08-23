996e50213db0e0a9befc7904ce805247af7174cb
yangoff <yangovip@gmail.com>
2017-08-23 13:07:22 -0300

    Volta ao banco original

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,6 @@
 import { Usuario } from '../usuario/usuario.model';
 import { Workshop } from './home-fb/workshop.model';
-<<<<<<< HEAD
 import { Component, OnInit,Input,Output,EventEmitter,AfterViewInit,OnDestroy,AfterViewChecked} from '@angular/core';
-=======
-import { Component, OnInit,Input,Output,EventEmitter,OnDestroy,AfterViewInit} from '@angular/core';
->>>>>>> 9cc239fed86404d4d20feacd9134d91c4c59a5f4
 import { ServiceComponent } from '../service/service.component'
 import { HomeFbModule} from '../home/home-fb/home-fb.module'
 import { AngularFireDatabase,FirebaseObjectObservable,FirebaseListObservable } from 'angularfire2/database';
@@ -22,14 +18,9 @@ import { MacService } from "../mac/mac.service";
   templateUrl: './home.component.html'
 })
 @Injectable()
-<<<<<<< HEAD
 export class HomeComponent implements OnInit {
   private tokenInterval: any;  
   hasIniti : boolean =false
-=======
-export class HomeComponent implements OnInit,OnDestroy,AfterViewInit{
-  hasIniti : boolean;
->>>>>>> 9cc239fed86404d4d20feacd9134d91c4c59a5f4
   @Output() eventAt:string=''
   @Output() workshopAt:string=''
   @Output() selecionaEvento =  new EventEmitter<String>()
@@ -42,22 +33,15 @@ export class HomeComponent implements OnInit,OnDestroy,AfterViewInit{
   items: FirebaseListObservable<any>;
   temp:Array<object>;
   ofAtual:string[]=[];
-  private tokenInterval: any;
   constructor(private usr:Usuario,private mService:MacService,private macs: MacsComponent,private angularFire: AngularFireDatabase,public hfb: HomeFbModule,private event:Event,private service:ServiceComponent) {
     this.eventos =this.hfb.snapdbEventos()
     this.hasIniti=false
   }
-<<<<<<< HEAD
   ngAfterViewChecked()
   {
      let tempoSeg = 1;
-     //this.tokenInterval = setInterval(this.initTheCount, tempoSeg * 1000);
-=======
-  ngAfterViewInit()
-  {
-     let tempoSeg = 1;
-     this.tokenInterval = setInterval(this.minhaF, tempoSeg*1000);
->>>>>>> 9cc239fed86404d4d20feacd9134d91c4c59a5f4
+     this.tokenInterval = setInterval(() => this.minhaF(), tempoSeg * 10000);
+     
 
   }
 
@@ -65,23 +49,24 @@ export class HomeComponent implements OnInit,OnDestroy,AfterViewInit{
   {
      if (!!this.tokenInterval) clearInterval(this.tokenInterval);
   }
-<<<<<<< HEAD
-
-
-
-=======
   
   private minhaF()
   {
         //this.macObs=this.mService.getMacs();
         //this.marcaEntrada(this.macObs)
-        this.prisnt('teste')
+        //this.asdgasd()
         
   }
 
-  private prisnt(str: string){ console.log(str);}
+  asdgasd(){
+    if(this.hasIniti==false){
+      console.log('false');
+      
+    }else{
+      console.log('true')
+    }
+  }
   
->>>>>>> 9cc239fed86404d4d20feacd9134d91c4c59a5f4
   onChangeEvent(){
     this.hfb.GLOBALEVENTKEY=this.selectEvent
     this.eventAt=this.hfb.getnameOfEvent()
@@ -95,16 +80,13 @@ export class HomeComponent implements OnInit,OnDestroy,AfterViewInit{
   }
 
   initTheCount(){
-    this.hasIniti=true
-<<<<<<< HEAD
-    this.macObs=this.mService.getMacs();
-    this.marcaEntrada(this.macObs)
-   //this.hfb.testeSET(this.workshopAt)
-    console.log('carniça');
-    
-=======
-    this.ngAfterViewInit();
->>>>>>> 9cc239fed86404d4d20feacd9134d91c4c59a5f4
+    if (this.selectWorkshop==undefined || this.selectEvent==undefined){  
+      alert('Favor selecionar um evento e uma oficina.')
+    } else {
+      this.hasIniti = true
+      this.macObs = this.mService.getMacs();
+      this.marcaEntrada(this.macObs)
+    }
   }
   cancelTheCount(){
     //console.log("Paroooooooooooo")
--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,16 +1,16 @@
 export const FirebaseConfig = {
-  /*
   apiKey: "AIzaSyDmOaIOhzHfD36CIwqghdq3oGPVvIgVqVY",
   authDomain: "blueme-3acca.firebaseapp.com",
   databaseURL: "https://blueme-3acca.firebaseio.com",
   projectId: "blueme-3acca",
   storageBucket: "blueme-3acca.appspot.com",
   messagingSenderId: "113538853283"
-  */
+  /*
   apiKey: "AIzaSyD2J3P5EaVnzgdXTUgkj9Laikby3o9UuLo",
   authDomain: "primeirocontatofirebase.firebaseapp.com",
   databaseURL: "https://primeirocontatofirebase.firebaseio.com",
   projectId: "primeirocontatofirebase",
   storageBucket: "primeirocontatofirebase.appspot.com",
   messagingSenderId: "943109408771"
+  */
 }
